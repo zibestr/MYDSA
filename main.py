22053f341a466b082ca08bae7f89f9137eb6adf7
Grander78498 <grander.l.l.1242@gmail.com>
2024-09-07 22:32:42 +0300

    Update cli_interface

---
+++ main.py
@@ -0,0 +1,28 @@
+from src.cli_interface.cli_interface import hello, mode, arg_inc_learn, arg_predict, arg_train
+import argparse
+import os
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog='mydsa', formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--type', choices=['default', 'predict', 'train', 'inc_learn'], default='default',
+                        help='predict - предсказать значения из одного ФАЙЛА формата .csv\n'
+                             'train - обучение модели на новых данных, находящихся в ДИРЕКТОРИИ\n'
+                             'inc_learn - дообучение модели на основе данных из ДИРЕКТОРИИ',
+                        metavar='')
+    parser.add_argument('--unsort', action="store_false",
+                        help='Отсортировать по какому-нибудь принципу')
+    parser.add_argument('--path', type=str, default=os.getcwd(),
+                        help='Путь до ФАЙЛА или ДИРЕКТОРИИ')
+    args = parser.parse_args()
+    if args.type == 'default':
+        hello()
+        mode()
+    else:
+        match args.type:
+            case 'predict':
+                arg_predict(args.path, args.unsort)
+            case 'train':
+                arg_train(args.path)
+            case 'inc_learn':
+                arg_inc_learn(args.path)
--- src/cli_interface/cli_interface.py
+++ src/cli_interface/cli_interface.py
@@ -14,10 +14,10 @@ from rich.table import Table
 
 import datetime
 import argparse
-from model import ModelInterface
+from src.model import ModelInterface
 
 
-time_column = 'Количество часов работы'
+time_column = 'failure_date'
 
 
 
@@ -33,7 +33,7 @@ def to_color(string, color):
 @click.command()
 @click.option("--t_mode", prompt="Введите тип процесса - \n1 - predict(предсказание)\n2 - train(обучение)\n3 - inc_learn(дообучение)\nq - выход\n->")
 def mode(t_mode):
-    print(psutil.virtual_memory().free)
+    # print(psutil.virtual_memory().free)
     if t_mode == "predict" or t_mode == "1":
         predict()
     elif t_mode == "train" or t_mode == "2":
@@ -64,22 +64,32 @@ def replace_df(delta):
 @click.option("--path", prompt="Введите путь файла, для которого будет происходит предсказание")
 def predict(path):
     if os.path.isfile(path) and path.endswith(".csv"):
-        print(Fore.GREEN+"Путь существует и есть файл нужного расширения, процесс запущен")
-        df = pd.read_csv(path)
-        size = df.shape
-        df = df.sort_values(time_column)
-        df[time_column] = df[time_column].apply(replace_df)
-        table = Table(title="Disks")
-        rows = df.iloc[:15].values.tolist()
-        rows = [[str(el) for el in row] for row in rows]
-        for column in df.columns:
-            table.add_column(column, vertical="middle")
-        for row in rows:
-            table.add_row(*row,  style='bright_green')
-        console = Console()
-        console.print(table)
-        print(Fore.YELLOW+"Получившиеся значения сохранены в файле result_{}.csv\nВсего количество элементов - {}".format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M"), size[0]))
-        df.to_csv("result_{}.csv".format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")), index=False)
+        if os.path.isfile('model/xgb.bin') and os.path.isfile('model/features.bin') and \
+           os.path.isfile('model/label.bin') and os.path.isfile('model/target.bin'):
+            model = ModelInterface('model/xgb.bin', 'model/label.bin', 'model/features.bin', 'model/target.bin')
+            try:
+                df = model.predict(path)
+            except ValueError:
+                print(Fore.RED + "CSV файл не подходящего формата")
+            print(Fore.GREEN+"Путь существует и есть файл нужного расширения, процесс запущен")
+            size = df.shape
+            df = df.sort_values(time_column)
+            df['time_delta'] = df[time_column] - datetime.datetime.strptime(os.path.basename(path).replace('.csv', ''), '%Y-%m-%d')
+            df['time_delta'] = df['time_delta'].apply(replace_df)
+            table = Table(title="Disks")
+            rows = df.iloc[:15].values.tolist()
+            rows = [[str(el) for el in row] for row in rows]
+            for column in df.columns:
+                table.add_column(column, vertical="middle")
+            for row in rows:
+                table.add_row(*row,  style='bright_green')
+            console = Console()
+            console.print(table)
+            print(Fore.YELLOW+"Получившиеся значения сохранены в файле result_{}.csv\nВсего количество элементов - {}".format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M"), size[0]))
+            df.to_csv("result_{}.csv".format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")), index=False)
+        else:
+            print(Fore.RED + "Модель не существует, нельзя выполнить предсказание. Сначала выполните обучение")
+        print(Fore.YELLOW+"")
         mode()
     else:
         print(Fore.RED+"Файл не того расширения или его не существует")
@@ -101,7 +111,11 @@ def train(path):
             print(Fore.YELLOW+"")
             train()
         else:
-            model = ModelInterface()
+            open('model/xgb.bin', 'a').close()
+            open('model/label.bin', 'a').close()
+            open('model/features.bin', 'a').close()
+            open('model/target.bin', 'a').close()
+            model = ModelInterface('model/xgb.bin', 'model/label.bin', 'model/features.bin', 'model/target.bin')
             files = [os.path.join(path, file) for file in files]
             print(Fore.GREEN+"Путь существует и есть файлы нужного расширения, процесс запущен")
             r2_score, mse = model.train(files, 'model/xgb.bin', 'model/label.bin', 'model/features.bin', 'model/target.bin')
@@ -145,7 +159,7 @@ def inc_learn(path):
         inc_learn()
     
 
-def arg_predict(path):
+def arg_predict(path, unsort):
     if os.path.isfile(path) and path.endswith(".csv"):
         if os.path.isfile('model/xgb.bin') and os.path.isfile('model/features.bin') and \
            os.path.isfile('model/label.bin') and os.path.isfile('model/target.bin'):
@@ -155,8 +169,9 @@ def arg_predict(path):
             except ValueError:
                 print(Fore.RED + "CSV файл не подходящего формата")
             size = df.shape
-            df = df.sort_values(time_column)
-            df['time_delta'] = df[time_column] - os.path.basename(path).replace('.csv', '')
+            if unsort:
+                df = df.sort_values(time_column)
+            df['time_delta'] = df[time_column] - datetime.datetime.strptime(os.path.basename(path).replace('.csv', ''), '%Y-%m-%d')
             df['time_delta'] = df['time_delta'].apply(replace_df)
             print(Fore.YELLOW+"Получившиеся значения сохранены в файле result_{}.csv\nВсего количество элементов - {}".format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M"), size[0]))
             df.to_csv("result_{}.csv".format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")), index=False)
@@ -167,7 +182,6 @@ def arg_predict(path):
 def arg_train(path):
     if os.path.isdir(path):
         files = []
-        print(os.listdir(path))
         for File in os.listdir(path):
             if File.endswith(".csv"):
                 files.append(File)
@@ -212,26 +226,26 @@ def hello():
     print(Fore.YELLOW+"")
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog='mydsa', formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('--type', choices=['default', 'predict', 'train', 'inc_learn'], default='default',
-                        help='predict - предсказать значения из одного ФАЙЛА формата .csv\n'
-                             'train - обучение модели на новых данных, находящихся в ДИРЕКТОРИИ\n'
-                             'inc_learn - дообучение модели на основе данных из ДИРЕКТОРИИ',
-                        metavar='')
-    parser.add_argument('--sort', action="store_true",
-                        help='Отсортировать по какому-нибудь принципу')
-    parser.add_argument('--path', type=str, default=os.getcwd(),
-                        help='Путь до ФАЙЛА или ДИРЕКТОРИИ')
-    args = parser.parse_args()
-    if args.type == 'default':
-        hello()
-        mode()
-    else:
-        match args.type:
-            case 'predict':
-                arg_predict(args.path)
-            case 'train':
-                arg_train(args.path)
-            case 'inc_learn':
-                arg_inc_learn(args.path)
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser(prog='mydsa', formatter_class=argparse.RawTextHelpFormatter)
+#     parser.add_argument('--type', choices=['default', 'predict', 'train', 'inc_learn'], default='default',
+#                         help='predict - предсказать значения из одного ФАЙЛА формата .csv\n'
+#                              'train - обучение модели на новых данных, находящихся в ДИРЕКТОРИИ\n'
+#                              'inc_learn - дообучение модели на основе данных из ДИРЕКТОРИИ',
+#                         metavar='')
+#     parser.add_argument('--sort', action="store_true",
+#                         help='Отсортировать по какому-нибудь принципу')
+#     parser.add_argument('--path', type=str, default=os.getcwd(),
+#                         help='Путь до ФАЙЛА или ДИРЕКТОРИИ')
+#     args = parser.parse_args()
+#     if args.type == 'default':
+#         hello()
+#         mode()
+#     else:
+#         match args.type:
+#             case 'predict':
+#                 arg_predict(args.path)
+#             case 'train':
+#                 arg_train(args.path)
+#             case 'inc_learn':
+#                 arg_inc_learn(args.path)
